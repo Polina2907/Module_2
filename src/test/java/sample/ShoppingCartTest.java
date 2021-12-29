4d33c3183793b9a7f319fd3b695ff42b7a805268
Polina2907 <polina2907polina@gmail.com>
2021-12-29 14:37:12 +0200

    added the name of the test

---
+++ src/test/java/sample/ShoppingCartTest.java
@@ -0,0 +1,16 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+class ShoppingCartTest {
+
+    @Test
+        void appendFormatted() {
+    }
+
+    @Test
+        void calculateDiscount() {
+
+    }
+}
