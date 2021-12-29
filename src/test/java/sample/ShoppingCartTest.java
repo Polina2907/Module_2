face3394ce1decc1c947dc999e3b4ec924f18f5e
Polina2907 <polina2907polina@gmail.com>
2021-12-29 14:40:27 +0200

    added tests

--- src/test/java/sample/ShoppingCartTest.java
+++ src/test/java/sample/ShoppingCartTest.java
@@ -7,10 +7,41 @@ class ShoppingCartTest {
 
     @Test
         void appendFormatted() {
+        StringBuilder sb = new StringBuilder();
+        ShoppingCart.appendFormatted(sb, "Test", 0, 10);
+        assertEquals("   Test    ", sb.toString());
+        sb = new StringBuilder();
+        ShoppingCart.appendFormatted(sb, "Test", 0, 2);
+        assertEquals("Te ", sb.toString());
+        sb = new StringBuilder();
+        ShoppingCart.appendFormatted(sb, "Test", 1, 15);
+        assertEquals("           Test ", sb.toString());
+        sb = new StringBuilder();
+        ShoppingCart.appendFormatted(sb, "Test", -1, 15);
+        assertEquals("Test            ", sb.toString());
     }
 
     @Test
         void calculateDiscount() {
-
+        // For NEW item discount is 0%;
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, 1));
+        assertEquals(10, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, 101));
+        assertEquals(80, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, 800));
+        assertEquals(80, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, 1000));
+        //For SALE item discount is 70%
+        assertEquals(80, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 500));
+        assertEquals(73, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 30));
+        assertEquals(71, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 10));
+        assertEquals(70, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 2));
+        //For each full 10 not NEW items item gets additional 1% discount, but not more than 80% total
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.NEW, 20));
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.NEW, 10));
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.NEW, 1));
+        //For SECOND_FREE item discount is 50% if quantity > 1
+        assertEquals(53, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 30));
+        assertEquals(51, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 10));
+        assertEquals(50, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 9));
+        assertEquals(50, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 2));
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 1));
     }
 }
